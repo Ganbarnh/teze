085c7f1cf75efd835055827f159ccb1dc1f44599
Ganbar Huseynli <ganbarnh@code.edu.az>
2023-01-06 01:18:44 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("A-ni daxil et:");
+
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("B-ni daxil et:");
+
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            double c = a + b;
+
+            Console.WriteLine($"{a}+{b}={c}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
